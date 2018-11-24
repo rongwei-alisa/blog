02d58ccfb65087b51b68f62470a07373a2eb7550
rongwei <rongwei_alisa@hotmail.com>
2018-11-24 15:25:08 +0800

    feat(Summary & Header): add logo and avatar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Avatar, Badge } from 'antd';
 import styles from './Header.module.less';
 
 const { Header } = Layout;
+const isLogin = true;
 
 function BlogHeader() {
   return (
     <Header className={styles.header}>
-      <div className={styles.logo}>BLOG</div>
+      <div className={styles.logo}>Alisa's Blog</div>
       <Menu
         theme="dark"
         mode="horizontal"
@@ -16,6 +17,17 @@ function BlogHeader() {
         <Menu.Item key="1">首页</Menu.Item>
         <Menu.Item key="2">Github</Menu.Item>
       </Menu>
+      <div className={styles.rightNav}>
+        {isLogin ?
+          <div>
+            <Badge dot title="您有新消息啦">
+              <Avatar alt="Avatar">Alisa</Avatar>
+            </Badge>
+          </div>
+          :
+          <div></div>
+        }
+      </div>
     </Header>
   );
 }
